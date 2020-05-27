25596e6f2699eee15e851e0ad3ea18ee1c000e7e
Steven Herbst <sgherbst@users.noreply.github.com>
2020-05-27 15:32:44 -0700

    make sure to exit the program at the end [skip ci]

--- uart_test.py
+++ uart_test.py
@@ -1,12 +1,22 @@
 import serial
 
+# connect to the CPU
+
 ser = serial.Serial(
     port='/dev/ttyUSB1',
     baudrate=115200
 )
 
+# read the ID
+
 ser.write('ID\n'.encode('utf-8'))
 out = ser.readline()
+
+# quit the program
+
+ser.write('EXIT\n'.encode('utf-8'))
+
+# check results
 
 val = int(out.strip())
 print(f'Got ID: {val}')
