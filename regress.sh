c40efdc17e7422e04c9dc993cc7eef586ccf6cc5
Steven Herbst <sgherbst@users.noreply.github.com>
2020-06-10 17:50:14 -0700

    [skip ci]

--- regress.sh
+++ regress.sh
@@ -6,12 +6,15 @@ pip install pyserial
 
 # build bitstream
 vivado -mode batch -source tcl/vivado.tcl
+tree
 
 # build firmware
 xsct tcl/sdk.tcl
+tree
 
 # program firmware
 xsct tcl/program.tcl
+tree
 
 # run UART test
 python uart_test.py
