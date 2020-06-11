0e47b636d2b3b0e54bce311e31b756a2d74d16bb
Steven Herbst <sgherbst@users.noreply.github.com>
2020-06-10 17:06:58 -0700

    [skip ci]

--- regress.sh
+++ regress.sh
@@ -9,7 +9,7 @@ vivado -mode batch -source tcl/vivado.tcl
 
 # export hardware to SDK
 mkdir -p project/project.sdk
-cp project/project.runs/impl_1/jtag_drv.xsa project/project.sdk/jtag_drv.xsa
+cp project/jtag_drv.xsa project/project.sdk/jtag_drv.xsa
 
 # build firmware
 xsct tcl/sdk.tcl
